b0f3fee60b09f2a820ec1f7b70ec414bcc5d193c
Naomi-Dennis <njdennis94@gmail.com>
2019-08-28 17:21:22 -0400

    Fix commit error. Add first batch of unit tests'

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper' 
 require 'tictactoe' 
 describe TicTacToe do 
+  context 'when the board is viewed' do
+    it 'returns a single list with 9 elements' do 
+     game = TicTacToe.new
+
+     expect(game.renderBoard.length).to eql 9
+    end 
+
+    it 'returns a list of all strings' do 
+     game = TicTacToe.new
+
+     expect(game.renderBoard.all?{ |space| space.class == String && space.length <= 1 } ).to eql true 
+    end 
+  end
+
   context 'when the game starts the board is empty' do 
     it 'sets the board to a list with empty strings' do 
         game = TicTacToe.new
