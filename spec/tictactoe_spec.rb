53d9626214df920db84372f2394db0c37b0dd9ad
Naomi-Dennis <njdennis94@gmail.com>
2019-08-28 17:17:49 -0400

    Create board test & begin implementing view board functionality

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper' 
 require 'tictactoe' 
 describe TicTacToe do 
+  context 'when the game starts the board is empty' do 
+    it 'sets the board to a list with empty strings' do 
+        game = TicTacToe.new
+        game.newGame 
+        expect( game.renderBoard.all?{ |space| space.s == '' } ).to eql true
+    end 
+  end   
 
 end 
