4e9abec8da1f1145af6f81bee28cb3d1abe9adf6
Naomi-Dennis <njdennis94@gmail.com>
2019-08-29 15:52:58 -0400

    Change #playerTurn to add token if there's a blank space versus an empty string

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -31,7 +31,7 @@ class TicTacToe
    desiredPosition = io.gets.to_i
    io.puts "Select a position to place your token [0 - 9]: "
 
-   unless board[desiredPosition - 1].empty? 
+   unless board[desiredPosition - 1] == ' ' 
       io.puts "That position is taken!" 
    else
     placeToken( desiredPosition )  
