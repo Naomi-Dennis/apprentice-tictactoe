ede8ffb3ce0e5336aea2896f2500e44641b407c5
Naomi-Dennis <njdennis94@gmail.com>
2019-09-04 12:12:12 -0400

    Fix formatting and remove uncessary whitespace in tictactoe spec and rb files

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,6 +1,7 @@
-class TicTacToe
+# frozen_string_literal: true
 
-  def initialize(io: )
+class TicTacToe
+  def initialize(io:)
     @board = Array.new(9, ' ')
     @current_token = 'X'
     @io = io
@@ -8,39 +9,38 @@ class TicTacToe
 
   def render_board
     io.puts "#{@board[0]}|#{@board[1]}|#{@board[2]}"
-    io.puts "------"
+    io.puts '------'
     io.puts "#{@board[3]}|#{@board[4]}|#{@board[5]}"
-    io.puts "------"
+    io.puts '------'
     io.puts "#{@board[6]}|#{@board[7]}|#{@board[8]}"
     board
   end
 
   def newGame
-   @board = Array.new(9, ' ')
+    @board = Array.new(9, ' ')
   end
 
- def place_token(position)
+  def place_token(position)
     board[position] = current_token
     render_board
- end
+  end
 
- def current_player
+  def current_player
     @current_token
- end
+  end
 
- def player_turn
-   desired_position = io.gets.to_i
-   io.puts "Select a position to place your token [1 - 9]: "
+  def player_turn
+    desired_position = io.gets.to_i
+    io.puts 'Select a position to place your token [1 - 9]: '
 
-   unless board[desired_position] == ' '
-      io.puts "That position is taken!"
-   else
-    place_token( desired_position )
-   end
+    if board[desired_position] == ' '
+      place_token(desired_position)
+    else
+      io.puts 'That position is taken!'
+     end
+  end
 
- end
+  private
 
- private
-
- attr_accessor :board, :current_token, :io
+  attr_accessor :board, :current_token, :io
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'tictactoe'
 ## Faker Classes
 class FakeIO
   attr_accessor :stdin, :stdout
-  def initialize(stdin: StringIO.new('') )
-      @stdin = stdin
-      @stdout = []
+  def initialize(stdin: StringIO.new(''))
+    @stdin = stdin
+    @stdout = []
   end
 
-  def puts( string )
+  def puts(string)
     @stdout << string
   end
 
@@ -17,33 +19,31 @@ class FakeIO
   end
 end
 
-def is_row_outputed( io: , row:  )
-    expect( io.stdout.include? row).to eql true
+def is_row_outputed(io:, row:)
+  expect(io.stdout.include?(row)).to eql true
 end
 
 describe TicTacToe do
-
   context 'when the board is rendered' do
     it 'returns a single list with 9 elements' do
-     game = TicTacToe.new(io: FakeIO.new)
+      game = TicTacToe.new(io: FakeIO.new)
 
-     expect(game.render_board.length).to eql 9
+      expect(game.render_board.length).to eql 9
     end
-
   end
 
   context 'when the game starts the board is empty' do
     it 'sets the board to a list with a single space' do
-        game = TicTacToe.new(io: FakeIO.new)
-        game.newGame
-        expect( game.render_board.all?{ |space| space == ' ' } ).to eql true
+      game = TicTacToe.new(io: FakeIO.new)
+      game.newGame
+      expect(game.render_board.all? { |space| space == ' ' }).to eql true
     end
   end
 
- context 'when a single token is placed on an empty board' do
+  context 'when a single token is placed on an empty board' do
     it 'the token will be "X"' do
       game = TicTacToe.new(io: FakeIO.new)
-      expect( game.current_player).to eql 'X'
+      expect(game.current_player).to eql 'X'
     end
 
     it 'the specified space will place the current token on the board' do
@@ -51,7 +51,7 @@ describe TicTacToe do
       game = TicTacToe.new(io: fakeIO)
       game.place_token(5)
 
-      is_row_outputed(io: fakeIO, row:" | |X")
+      is_row_outputed(io: fakeIO, row: ' | |X')
     end
 
     it 'the token can be placed anywhere on the board' do
@@ -59,28 +59,27 @@ describe TicTacToe do
       game = TicTacToe.new(io: fakeIO)
 
       game.place_token(3)
-      is_row_outputed(io: fakeIO, row:"X| | " )
+      is_row_outputed(io: fakeIO, row: 'X| | ')
 
       game.newGame
       fakeIO.stdout = []
       game.place_token(5)
-      is_row_outputed(io: fakeIO, row: " | |X" )
-
+      is_row_outputed(io: fakeIO, row: ' | |X')
 
       game.newGame
       fakeIO.stdout = []
       game.place_token(7)
-      is_row_outputed(io: fakeIO, row: " |X| ")
+      is_row_outputed(io: fakeIO, row: ' |X| ')
     end
 
     it 'renders the board after a token is placed' do
       fakeIO = FakeIO.new
       game = TicTacToe.new(io: fakeIO)
 
-       game.place_token(3)
-       expectedBoardOutput = [" | | ", "------", "X| | ", "------", " | | "]
+      game.place_token(3)
+      expectedBoardOutput = [' | | ', '------', 'X| | ', '------', ' | | ']
 
-       expect( fakeIO.stdout ).to eql expectedBoardOutput
+      expect(fakeIO.stdout).to eql expectedBoardOutput
     end
   end
 
@@ -89,29 +88,29 @@ describe TicTacToe do
       desired_position = 4
 
       user_input = StringIO.new(desired_position.to_s)
-      fakeIO = FakeIO.new( stdin: user_input )
-      game = TicTacToe.new(io: fakeIO )
+      fakeIO = FakeIO.new(stdin: user_input)
+      game = TicTacToe.new(io: fakeIO)
 
       game.player_turn
 
-      is_row_outputed(io: fakeIO, row: " |X| ")
+      is_row_outputed(io: fakeIO, row: ' |X| ')
     end
   end
 
- context 'when the user tries to place a token in a free space' do
+  context 'when the user tries to place a token in a free space' do
     it 'renders the board with their token in the specified space' do
       desired_position = 4
 
       user_input = StringIO.new(desired_position.to_s)
-      fakeIO = FakeIO.new( stdin: user_input )
-      game = TicTacToe.new(io: fakeIO )
+      fakeIO = FakeIO.new(stdin: user_input)
+      game = TicTacToe.new(io: fakeIO)
 
-      expect(game.render_board[desired_position] ).to eql ' '
+      expect(game.render_board[desired_position]).to eql ' '
 
       game.player_turn
 
-      is_row_outputed(io: fakeIO, row: " |X| ")
+      is_row_outputed(io: fakeIO, row: ' |X| ')
       expect(game.render_board[desired_position]).to eql 'X'
     end
- end
+  end
 end
