9eb9c3be6ef24df27e0bc65feed01d817824f7ae
Naomi-Dennis <naomi@8thlight.com>
2019-09-27 09:47:52 -0400

    In presenter_spec/class: Add #prompt_select_position

--- lib/presenter.rb
+++ lib/presenter.rb
@@ -1,2 +1,10 @@
 class Presenter
+  def initialize(io:)
+    @io = io
+  end
+
+  def prompt_select_position
+    io.puts 'Select a position to place your token [1 - 9]:'
+  end
+
 end
--- spec/presenter_spec.rb
+++ spec/presenter_spec.rb
@@ -2,6 +2,22 @@ require 'spec_helper'
 require 'presenter'
 require 'board'
 
+class FakeIO
+  def initialize(stdout: [])
+    @stdout = stdout
+  end
+
+  def self.puts(*output)
+    @stdout << output.join("\n")
+    @stdout
+  end
+end
 
 describe Presenter do
+  it 'prompts the player to select a position' do
+    presenter = Presenter.new(io: FakeIO.new)
+    output = presenter.prompt_select_position
+    expect(output).to include(/select.*position/i)
+  end
+
 end
