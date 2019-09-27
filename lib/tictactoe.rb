7560cb587f10fe09e05eb50f83f3af24000d849a
Naomi-Dennis <naomi@8thlight.com>
2019-09-27 16:15:37 -0400

    In tictactoe.rb: Minor Changes * Prompt player for position if input is invalid via presenter * Apply presenter to #prompt_user_for_input * Remove #validate * Add validator and tokens to private attr_accessor

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -27,9 +27,12 @@ class TicTacToe
   end
 
   def begin_player_turn
-    desired_position = prompt_user_for_input
-    input_is_valid = validate(user_input: desired_position, board: @board)
-    place_token(desired_position) if input_is_valid
+    position = prompt_user_for_input
+    input_is_valid = validator.check(position: position, board: @board, view: @presenter)
+    place_token(position) if input_is_valid
+    presenter.prompt_select_another_position unless input_is_valid
+    render_board
+  end
 
   def setup
     render_board
@@ -37,19 +40,13 @@ class TicTacToe
 
   private
 
-  attr_accessor :board, :current_token, :io, :presenter
+  attr_accessor :board, :current_token, :io, :presenter, :validator, :tokens
 
   def prompt_user_for_input
+    presenter.show_player_turn(player: current_token)
     presenter.prompt_select_position
     desired_position = io.gets.to_i
     desired_position
-  end
-
-  def validate(user_input:, board:)
-    presenter.tell_position_invalid unless board.has(position: user_input)
-    presenter.tell_position_taken if board.occupied_at(position: user_input)
-    presenter.prompt_select_another_position
-    board.has(position: user_input) && !board.occupied_at(position: user_input)
   end
 
   def switch_turn
