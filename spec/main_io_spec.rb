8af9f50ff1ee864666b82d9a355039ec6c4968dc
Naomi-Dennis <naomi@8thlight.com>
2019-10-02 15:11:22 -0400

    In main_io_spec: Update constructor names & renaming of #gets and #puts

--- spec/main_io_spec.rb
+++ spec/main_io_spec.rb
@@ -37,8 +37,8 @@ describe MainIO do
     it 'returns captured string' do
       test_input = '1'
       stdin = FakeStdin.new(string: test_input)
-      io = MainIO.new(stdin: stdin, stdout: FakeStdout.new)
-      input_result = io.gets
+      io = MainIO.new(input_stream: stdin, output_stream: FakeStdout.new)
+      input_result = io.get_keyboard_input
       expect(input_result).to eql test_input
     end
   end
@@ -46,15 +46,15 @@ describe MainIO do
   context 'when showing output' do
     it 'returns the captured string' do
       test_output = 'this is a test output'
-      io = MainIO.new(stdin: FakeStdin.new, stdout: FakeStdout.new)
-      output_result = io.puts test_output
+      io = MainIO.new(input_stream: FakeStdin.new, output_stream: FakeStdout.new)
+      output_result = io.output_to_screen test_output
       expect(output_result).to eql test_output
     end
 
     context 'when multiple strings are passed' do
       it 'returns all strings separated by new lines' do
-        io = MainIO.new(stdin: FakeStdin.new, stdout: FakeStdout.new)
-        output_result = io.puts 'this', 'is', 'a', 'test'
+        io = MainIO.new(input_stream: FakeStdin.new, output_stream: FakeStdout.new)
+        output_result = io.output_to_screen 'this', 'is', 'a', 'test'
         expect(output_result).to eql "this\nis\na\ntest"
       end
     end
