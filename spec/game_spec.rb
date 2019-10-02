75d892cfdcdedb9bc31bd78f4c02487c941747ab
Naomi-Dennis <naomi@8thlight.com>
2019-10-02 15:09:40 -0400

    In game_spec.rb: Add additional parameters for updated functions & Add FakePresenter & FakeBoard

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 require 'game'
-
+require 'fake_io'
 class FakeLogic
   attr_accessor :game_over_called
 
@@ -11,21 +11,28 @@ class FakeLogic
     @game_over_loops = game_over_loops
   end
 
-  def game_over?
+  def game_over?(presenter:, board:)
     @game_over_called += 1
     @game_over_loops.shift
   end
 
-  def begin_player_turn; end
+  def begin_player_turn(board:, controller:, presenter:); end
 
   def setup; end
 end
+
+class FakePresenter
+  def show_board(board:); end
+end
+
+class FakeBoard; end
+
 
 describe Game do
   it 'loops through game logic until the game is over' do
     game_loops = [false, false, false, false, false, true]
     game = FakeLogic.new(game_over_loops: game_loops)
-    Game.play(game_logic: game)
+    Game.play(controller: FakeIO.new, board: FakeBoard.new, presenter: FakePresenter.new, logic: game)
     expect(game.game_over_called).to be 6
   end
 end
