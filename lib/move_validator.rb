ee6989528ae7caaeb97d8a9ee94914e3956e9fff
Naomi-Dennis <naomi@8thlight.com>
2019-10-02 15:03:53 -0400

    Delete move_validator.rb

--- lib/move_validator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class MoveValidator
-  def self.check(position:, board:, view:)
-    view.tell_position_taken if board.occupied_at(position: position)
-    view.tell_position_invalid unless board.has(position: position)
-    board.has(position: position) && !board.occupied_at(position: position)
-  end
-end
