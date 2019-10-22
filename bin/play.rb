ba87fd02b3881b95c99e602e87821be9ca38d039
Naomi-Dennis <naomi@8thlight.com>
2019-10-22 11:14:19 -0400

    Refactor play.rb

--- bin/play.rb
+++ bin/play.rb
@@ -1,5 +1,15 @@
-# frozen_string_literal: truei
+# frozen_string_literal: true
 
-require_relative './tictactoe_setup'
+require_relative '../lib/main_io.rb'
+require_relative '../lib/presenter.rb'
+require_relative '../lib/user_input.rb'
+require_relative '../lib/tictactoe_setup.rb'
 
-Game.play(TicTacToeSetup::CARTRIDGE)
+io = MainIO.new(input_stream: $stdin, output_stream: $stdout)
+
+tictactoe_configuration = {
+  setup: TicTacToeSetup.new(user_input: UserInput.new(io: io)),
+  presenter: Presenter.new(io: io)
+}
+
+Game.start(tictactoe_configuration)
