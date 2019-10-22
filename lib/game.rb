db0ed139f38d065429d9e6cab037afb20880a58a
Naomi-Dennis <naomi@8thlight.com>
2019-10-22 11:13:53 -0400

    Add #start to game to build and play game

--- lib/game.rb
+++ lib/game.rb
@@ -7,8 +7,11 @@ class Game
       presenter.show_player_turn(player: logic.current_player_token)
       presenter.prompt_select_position
       player_error_messages = logic.begin_player_turn(board: board)
-      display_player_error_messages(presenter: presenter, messages: player_error_messages)
+
+      display_player_error_messages(presenter: presenter,
+                                    messages: player_error_messages)
     end
+
     presenter.show_board(board: board)
   end
 
@@ -19,5 +22,12 @@ class Game
     end
   end
 
-  private_class_method :display_player_error_messages
+  def self.start(setup:, presenter:)
+    menu = setup.create_game_mode_menu
+    presenter.show_numbered_menu(menu: menu)
+    game_mode_selected = menu.accept_user_selection
+    game_mode = setup.build(mode: game_mode_selected, presenter: presenter)
+    play(game_mode)
+  end
+
 end
